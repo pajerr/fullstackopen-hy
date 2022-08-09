14d68e6f34747fe80db62fdaf89fed3962a7440b
Erno <54817547+pajerr@users.noreply.github.com>
2022-08-09 22:40:14 +0300

    edited uncafe 1.5

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -34,21 +34,18 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [total, setTotal] = useState(0);
+  const total = good + neutral + bad;
 
   const handleGoodClick = () => {
     setGood(good + 1);
-    setTotal(total + 1);
   };
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1);
-    setTotal(total + 1);
   };
 
   const handleBadClick = () => {
     setBad(bad + 1);
-    setTotal(total + 1);
   };
 
   return (
