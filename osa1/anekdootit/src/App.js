8449c464dd0a4c5606bf0f1fc8899810327bcb68
Erno <54817547+pajerr@users.noreply.github.com>
2022-08-13 13:47:24 +0300

    anekdootit

--- osa1/anekdootit/src/App.js
+++ osa1/anekdootit/src/App.js
@@ -2,7 +2,6 @@ import { useState } from "react";
 
 //otetaan anecdootti array vastaan ja palautetaan random anekdootti listalta
 const DisplayAnecdote = ({ anecdotes, selected }) => {
-  //const randomInt = getRandomInt(0, 5);
   return <div>{anecdotes[selected]}</div>;
 };
 
@@ -15,6 +14,16 @@ const getRandomInt = function (min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 };
+const VoteStats = ({ selected, points, totalVotes }) => {
+  console.log("points[selected] arvo:", points[selected]);
+  //kutsutaan funktiota joka tarkistaa onko points tyhja
+  //const pointsEmpty = noVotes(points);
+  if (points[selected] === 0) {
+    return <div>No votes yet</div>;
+  } else {
+    return <div>Has {points[selected]} votes</div>;
+  }
+};
 
 const App = () => {
   const anecdotes = [
@@ -26,19 +35,42 @@ const App = () => {
     "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
     "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.",
   ];
-
   const [selected, setSelected] = useState(0);
+  const [allVotes, setAll] = useState(0);
+  //let points = { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 };
+  //let points = new Array(6).fill(0);
+  const [points, setPoints] = useState(new Array(6).fill(0));
 
-  const handleClick = () => {
-    console.log(getRandomInt(0, 6));
-    setSelected(getRandomInt(0, 6));
-    <DisplayAnecdote anecdotes={anecdotes} selected={getRandomInt()} />;
+  /*
+  const handleClick = ({ anecdotes, selected }) => {
+    const newSelected = selected;
+    setSelected(newSelected);
+    <DisplayAnecdote anecdotes={anecdotes} selected={newSelected} />;
+  };
+*/
+  const handleVoteClick = ({ selected, points }) => {
+    console.log("points array in voteClick:", points[selected]);
+    let newPoints = points;
+    points[selected] += 1;
+    setPoints(newPoints);
+    console.log("VOTED...");
+    console.log(
+      "points in vote array in voteClickComponent:",
+      points[selected]
+    );
   };
 
+  //<updateSelected randomInt={getRandomInt(0, 6)} />
   return (
     <div>
       <DisplayAnecdote anecdotes={anecdotes} selected={selected} />
-      <Button handleClick={handleClick} text="Next anecdote" />
+      <VoteStats points={points} anecdote={selected} />
+      <Button
+        handleClick={() => {
+          setSelected(getRandomInt(0, 6));
+        }}
+        text="Next anecdote"
+      />
     </div>
   );
 };
