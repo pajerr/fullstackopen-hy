1480ed3ff44ca6a80b84f753491894a1367a6803
Erno <54817547+pajerr@users.noreply.github.com>
2022-08-15 21:11:35 +0300

    puhelinluettelo part1

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addName = (event) => {
+    event.preventDefault();
+    const nameObject = {
+      name: newName,
+      id: persons.length + 1,
+    };
+    console.log("button clicked", event.target);
+
+    setPersons(persons.concat(nameObject));
+    setNewName("");
+  };
+
+  //tapahumankäsittelijämetodi saa parametrina tapahtumaolion event
+  //tapahtumaolion target viittaa inputin syötekentään
+  //value viittaa inputin syötekentän arvoon
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      {/* <div>new name debug: {newName}</div> */}
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => (
+          <li key={person.id}>{person.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
