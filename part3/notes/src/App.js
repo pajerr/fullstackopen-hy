dc3c14bc6445a8276f182798b7e1184209e67755
Erno <54817547+pajerr@users.noreply.github.com>
2022-11-07 19:44:40 +0200

    part3 example done

---
+++ part3/notes/src/App.js
@@ -0,0 +1,148 @@
+import { useState, useEffect } from "react";
+
+import Note from "./components/Note";
+import noteService from "./services/notes";
+
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null;
+  }
+
+  return <div className="error">{message}</div>;
+};
+
+const Footer = () => {
+  const footerStyle = {
+    color: "green",
+    fontStyle: "italic",
+    fontSize: 16,
+  };
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>
+        Note app, Department of Computer Science, University of Helsinki 2022
+      </em>
+    </div>
+  );
+};
+
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  //Lisätään komponentille App tila newNote lomakkeen syötettä varten ja määritellään se input-komponentin attribuutin value arvoksi:
+  const [newNote, setNewNote] = useState("");
+  //state that keeps track of of notes to display
+  const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    //noteService returns response.data from the promise returned by axios.get
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
+    });
+  }, []);
+  //[] fetches only once when the component is rendered for the first time
+
+  //Tapahtumankäsittelijä kutsuu heti tapahtuman metodia event.preventDefault() jolla se estää lomakkeen lähetyksen oletusarvoisen toiminnan, joka aiheuttaisi mm. sivun uudelleenlatautumisen.
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      //sisältökentän arvo saadaan komponentin tilasta newNote
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+    };
+
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
+      setNewNote("");
+    });
+  };
+
+  //Jotta kontrolloidun syötekomponentin editoiminen olisi mahdollista, täytyy sille rekisteröidä tapahtumankäsittelijä, joka synkronoi syötekenttään tehdyt muutokset komponentin App tilaan:
+
+  //Tapahtumaolion kenttä target vastaa nyt kontrolloitua input-kenttää ja event.target.value viittaa
+  //inputin syötekentän arvoon.
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  //The items of the list depend on the state of the component
+
+  /*
+  The displayed notes (all versus important) are controlled with a button. 
+  The event handler for the button is so simple that it has been defined directly
+   in the attribute of the button element. 
+   The event handler switches the value of showAll from true to false and vice versa:
+  */
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => note.important === true);
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+    /*
+The callback function sets the component's notes state to a new array that contains all 
+the items from the previous notes array, except for the old note which is replaced 
+by the updated version of it returned by the server:
+*/
+    noteService
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
+      })
+      .catch((error) => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        );
+        /*
+Removing an already deleted note from the application's state is done with the array filter method, 
+which returns a new array comprising only of the items from the list for which the function
+ that was passed as a parameter returns true for:
+*/
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
+        setNotes(notes.filter((n) => n.id !== id));
+      });
+  };
+
+  /*
+ if note.id !== id is true; we simply copy the item from the old array into the new array. 
+ If the condition is false, then the note object returned by the server is added to the 
+ array instead.
+*/
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <Notification message={errorMessage} />
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        {/*Lomakkeen input-komponentille on nyt rekisteröity tapahtumankäsittelijä
+        tilanteeseen onChange:
+        */}
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ part3/phonebook/src/App.js
@@ -0,0 +1,242 @@
+import { useState, useEffect } from 'react'
+
+import personService from './services/persons.js'
+
+function alreadyInPersons(persons, first, last) {
+    //return first element of array that matches first and last, otherwise return false
+    return persons.find(
+        (person) => person.name.includes(first) && person.name.includes(last)
+    )
+}
+
+const NewEntry = ({
+    newName,
+    newNumber,
+    handleNameChange,
+    handleNumberChange,
+    addPerson
+}) => {
+    return (
+        <form onSubmit={addPerson}>
+            <div>
+                name:
+                <input value={newName} onChange={handleNameChange} />
+            </div>
+            <div>
+                number:
+                <input value={newNumber} onChange={handleNumberChange} />
+            </div>
+            <div>
+                <button type="submit">add</button>
+            </div>
+        </form>
+    )
+}
+
+const DisplayPersons = ({ persons, filter, handlePersonDelete }) => {
+    //display all persons if filter is empty, otherwise filter persons
+    if (filter.length === 0) {
+        return (
+            <div>
+                <ul>
+                    {persons.map((person) => (
+                        <li key={person.id}>
+                            {person.name} {person.number}
+                            <button
+                                onClick={() => handlePersonDelete(person.id)}
+                            >
+                                Delete person
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+    } else {
+        const filteredPersons = persons.filter((person) =>
+            person.name.toLowerCase().includes(filter.toLowerCase())
+        )
+        return (
+            <div>
+                <ul>
+                    {filteredPersons.map((person) => (
+                        <li key={person.id}>
+                            {person.name} {person.number}
+                            <button
+                                onClick={() => handlePersonDelete(person.id)}
+                            >
+                                Delete person
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
+}
+
+const Filter = ({ newFilter, handleFilterChange }) => {
+    return (
+        <div>
+            <input value={newFilter} onChange={handleFilterChange} />
+        </div>
+    )
+}
+/*
+If the value of the message prop is null, then nothing is rendered to the screen, 
+and in other cases the message gets rendered inside of a div element.
+*/
+const Notification = ({ message }) => {
+    if (message === null) {
+        return null
+    }
+
+    return <div className="status">{message}</div>
+}
+
+const App = () => {
+    const [persons, setPersons] = useState([])
+    const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
+    const [newFilter, setFilter] = useState('')
+    const [statusMessage, setStatusMessage] = useState(null)
+
+    useEffect(() => {
+        personService.getAll().then((initialPersons) => {
+            setPersons(initialPersons)
+        })
+    }, [])
+
+    //now this add new person and update existing person
+    const addPerson = (event) => {
+        //broken
+        event.preventDefault()
+        //check if person already exists
+        let [first, last] = newName.split(' ')
+        // extract person from functions return value
+        const existingPerson = alreadyInPersons(persons, first, last)
+
+        // if person exists and returned from function
+        if (existingPerson) {
+            console.log('entered if existingPerson')
+            let result = window.confirm(
+                `${first} ${last} is already added to phonebook, replace the old number with a new one?`
+            )
+
+            //if confirm yes to update number
+            if (result === true) {
+                //create new person object with updated number
+                const personObject = {
+                    name: existingPerson.name,
+                    number: newNumber,
+                    id: existingPerson.id
+                }
+
+                //update number
+                //person.id is from person returned from function
+                //personObject is from form data that has new number
+                personService
+                    //.update(existingPerson.id, personObject.number)
+                    .update(personObject)
+                    //updatedNumber is response from server
+                    .then((resUpdatedPersonObject) => {
+                        //create new personObject with updated number and use existing id
+                        const updatedPerson = {
+                            ...existingPerson,
+                            number: resUpdatedPersonObject.number
+                        }
+                        console.log('updatedPerson debug: ', updatedPerson)
+                        setPersons(
+                            persons.map((person) =>
+                                person.id !== existingPerson.id
+                                    ? person
+                                    : updatedPerson
+                            )
+                        )
+                        setNewName('')
+                        setNewNumber('')
+                        setStatusMessage(`Updated ${first} ${last} number`)
+                        setTimeout(() => {
+                            setStatusMessage(null)
+                        }, 3000)
+                    })
+                console.log('updated number...')
+            } else {
+                //do nothing
+                console.log('dont update...')
+            }
+            //alert(`${first} ${last} is already added to phonebook`);
+        }
+        //create new person object
+        else {
+            const personObject = {
+                name: newName,
+                number: newNumber,
+                id: persons.length + 1
+            }
+            personService.create(personObject).then((returnedPerson) => {
+                setPersons(persons.concat(returnedPerson))
+                setNewName('')
+                setNewNumber('')
+                setStatusMessage(`Added ${first} ${last} to phonebook`)
+                setTimeout(() => {
+                    setStatusMessage(null)
+                }, 3000)
+            })
+        }
+    }
+
+    const handleNameChange = (event) => {
+        console.log(event.target.value)
+        setNewName(event.target.value)
+    }
+
+    const handleNumberChange = (event) => {
+        console.log(event.target.value)
+        setNewNumber(event.target.value)
+    }
+
+    const handleFilterChange = (event) => {
+        console.log(event.target.value)
+        setFilter(event.target.value)
+    }
+
+    const handlePersonDelete = (id) => {
+        console.log(`delete person with id ${id}`)
+        personService.remove(id)
+        //need to update persons state and remove person with id that was deleted
+        setPersons(persons.filter((person) => person.id !== id))
+    }
+
+    /*const personstoShow = showAll
+    ? person
+    : persons.filter((person) => person.includes(filter));*/
+
+    return (
+        <div>
+            <h1>Phonebook</h1>
+            <Notification message={statusMessage} />
+            <h2>Add new entry</h2>
+            <NewEntry
+                newName={newName}
+                newNumber={newNumber}
+                handleNameChange={handleNameChange}
+                handleNumberChange={handleNumberChange}
+                addPerson={addPerson}
+            />
+            <h2>Filter</h2>
+            <Filter
+                newFilter={newFilter}
+                handleFilterChange={handleFilterChange}
+            />
+            <h2>Numbers</h2>
+            <DisplayPersons
+                persons={persons}
+                filter={newFilter}
+                handlePersonDelete={handlePersonDelete}
+            />
+        </div>
+    )
+}
+
+export default App
