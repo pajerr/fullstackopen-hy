b17c6236f422f942cea1a0c178f540b3dc102c62
Erno <54817547+pajerr@users.noreply.github.com>
2022-11-08 18:55:08 +0200

    push updated notes

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 
+var morgan = require('morgan')
+const middleware = morgan('tiny')
+
+const cors = require('cors')
+app.use(cors())
+
+app.use(express.static('build'))
+
 let notes = [
     {
         id: 1,
@@ -33,6 +41,8 @@ const requestLogger = (request, response, next) => {
 app.use(express.json())
 
 app.use(requestLogger)
+
+app.use(middleware)
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
@@ -92,7 +102,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
