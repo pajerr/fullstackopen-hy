2339fbad84518275ab5cb61ee6e7c718ad4202e3
Erno <54817547+pajerr@users.noreply.github.com>
2022-11-20 21:05:25 +0200

    push part4 example with comments

---
+++ part4/phonebook/src/services/persons.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+const baseUrl = 'api/persons'
+
+const getAll = () => {
+    const request = axios.get(baseUrl)
+    return request.then((response) => response.data)
+}
+
+const create = (newObject) => {
+    const request = axios.post(baseUrl, newObject)
+    return request.then((response) => response.data)
+}
+
+const update = (updatedPerson) => {
+    const request = axios.put(`${baseUrl}/${updatedPerson.id}`, {
+        name: updatedPerson.name,
+        number: updatedPerson.number,
+        id: updatedPerson.id
+    })
+    //return only the updated number, ID stays the same
+    return request.then((response) => response.data)
+}
+
+// const update = (id, updatedNumber) => {
+//     const request = axios.put(`${baseUrl}/${id}`, { number: updatedNumber })
+//     //return only the updated number, ID stays the same
+//     return request.then((response) => response.data.number)
+// }
+
+const remove = (id) => {
+    const request = axios.delete(`${baseUrl}/${id}`)
+}
+
+export default { getAll, create, update, remove }
